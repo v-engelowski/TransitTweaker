492f2c14d3422b5717c84ec61c561ab004f23c57
MakerMacher <admin@makermacher.net>
2025-05-10 02:34:17 +0200

    update package.json and mod.ts for version bump and dependency adjustments

--- src/mod.ts
+++ src/mod.ts
@@ -2,30 +2,29 @@ import { DependencyContainer } from "tsyringe";
 import { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
 import { ILogger } from "@spt/models/spt/utils/ILogger";
 import { DatabaseServer } from "@spt/servers/DatabaseServer";
-import { VFS } from "@spt/utils/VFS";
+import { FileSystemSync } from "@spt/utils/FileSystemSync";
 import { LogTextColor } from "@spt/models/spt/logging/LogTextColor";
 import path from "path";
 import { JsonUtil } from "@spt/utils/JsonUtil";
 import { IDatabaseTables } from "@spt/models/spt/server/IDatabaseTables";
-import { Ixyz } from "@spt/models/eft/common/Ixyz";
 
 
 class TransitTweaker implements IPostDBLoadMod {
     private config: any;
     private logger: ILogger;
     private databaseServer: DatabaseServer;
-    private vfs: VFS;
+    private fileSystem: FileSystemSync;
     private jsonUtil: JsonUtil;
     private db: IDatabaseTables;
 
     public postDBLoad(container: DependencyContainer): void {
         this.logger = container.resolve<ILogger>("WinstonLogger");
-        this.vfs = container.resolve<VFS>("VFS");
+        this.fileSystem = container.resolve<FileSystemSync>("FileSystemSync");
         this.jsonUtil = container.resolve<JsonUtil>("JsonUtil");
         this.databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
         this.db = this.databaseServer.getTables();
 
-        this.config = this.jsonUtil.deserializeJson5(this.vfs.readFile(path.join(__dirname , "../config/config.json5")));
+        this.config = this.jsonUtil.deserializeJson5(this.fileSystem.read(path.join(__dirname , "../config/config.json5")));
 
         // Globals config
         const transitConfig = this.db.globals.config.TransitSettings;
